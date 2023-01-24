b7f711e2e5379b7335fa693b554b852b59289e1b
Corentin Chatelin <corentin.chatelin@lgce.fr>
2023-01-24 17:36:19 -0500

    WIP 2: textes ajoutés + bleu léger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $( '#sticky-top-navbar .navbar-nav a' ).on( 'click', function () {
-    console.log('bite');
     $( '#sticky-top-navbar .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
     $( this ).parent( 'li' ).addClass( 'active' );
+});
+
+$("#btnToggleKine").click(function(){
+    $("#collapseOsteo").removeClass("show");
+    $("#collapseCabinet").removeClass("show");
+});
+
+$("#btnToggleOsteo").click(function(){
+    $("#collapseKine").removeClass("show");
+    $("#collapseCabinet").removeClass("show");
+});
+
+$("#btnToggleCabinet").click(function(){
+    $("#collapseKine").removeClass("show");
+    $("#collapseOsteo").removeClass("show");
 });
